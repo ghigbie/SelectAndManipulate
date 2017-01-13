087f50f8f3d6d48cd7ae53d7f52fba9add6ec2f0
George Higbie <gehigbie@gmail.com>
2017-01-12 18:27:32 -0700

    made changes to js files

--- hello.js
+++ hello.js
@@ -8,10 +8,18 @@ console.log(tag2);
 console.log(tag3);
 console.log(tag4);
 
-var tag5 = document.querySelector("#highlight");
+var tag5 = document.querySelector("#highlight"); //uses CSS selectors
 var tag6 = document.querySelector(".bolded");
 var tag7 = document.querySelector("h1");
 
 console.log(tag5);
 console.log(tag6);
 console.log(tag7);
+
+var tag8 = document.querySelectorAll(".bolded");
+
+console.log(tag8);
+
+var tag9 = document.querySelectorAll("h1");
+
+console.log(tag9);
